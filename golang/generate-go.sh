2d5a7cac4f39ab69ba062d8fbd7be161941d7dcc
Nils Diekmann <github@kinneko.de>
2023-06-13 04:29:18 +0200

    - generate.go supports readme.md - fixed deleting of generated code

--- golang/generate-go.sh
+++ golang/generate-go.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
+outdated=( $(ls -d */) )
+for i in "${outdated[@]}"
+do
+    rm -r ${i}
+    echo "Removed generated go code for $i"
+done
+
 cd ..
 
 export PATH="$PATH:$(go env GOPATH)/bin"
 
 protobase=proto
 protopath=${protobase}/kinnekode
-projects=( $(ls ${protopath}) )
+projects=( $(ls -d ${protopath})/*/ )
 outputpath=golang
 
 for i in "${projects[@]}"
 do
-    find ${protobase}/${i} -name *.pb.go \
-    -exec rm {} +
-    echo "Removed generated go code"
-
-    find ${protopath}/${i} -name *.proto \
+    find ${i} -name *.proto \
     -exec tools/protoc/win64/bin/protoc.exe --proto_path=${protobase} --go_out=${outputpath} --go_opt=paths=source_relative --go-grpc_out=${outputpath} --go-grpc_opt=paths=source_relative {} +
 
     echo "Generated go code for: $i"
