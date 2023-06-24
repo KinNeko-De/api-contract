22cb5133c0f9eb013c7fe72d329d90e4da122039
Nils Diekmann <github@kinneko.de>
2023-06-24 23:19:34 +0200

    - test for dotnet decimal

--- dotnet/protobuf/KinNekoDe.ApiContract.Protobuf/Decimal.cs
+++ dotnet/protobuf/KinNekoDe.ApiContract.Protobuf/Decimal.cs
@@ -1,23 +1,22 @@
-﻿using Google.Protobuf;
+﻿using System.Globalization;
+using Google.Protobuf;
 
 namespace Kinnekode.Protobuf;
-
-
 public partial class Decimal : ICustomDiagnosticMessage
 {
     public static Decimal FromDecimal(decimal value)
     {
-        return new Decimal { Value = ToMessageValue(value) };
+        return new Decimal() { Value = ToMessageValue(value) };
     }
 
     public decimal ToDecimal()
     {
-        return decimal.Parse(Value);
+        return decimal.Parse(Value, GetNumberStyles(), CultureInfo.InvariantCulture);
     }
 
     public bool TryParseToDecimal(out decimal value)
-    {
-        if (decimal.TryParse(Value, out decimal parsedDecimal))
+    { ;
+        if (decimal.TryParse(Value, GetNumberStyles(), CultureInfo.InvariantCulture, out decimal parsedDecimal))
         {
             value = parsedDecimal;
             return true;
@@ -37,6 +36,11 @@ public partial class Decimal : ICustomDiagnosticMessage
 
     private static string ToMessageValue(decimal value)
     {
-        return value.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture);
+        return value.ToString(CultureInfo.InvariantCulture);
+    }
+
+    private NumberStyles GetNumberStyles()
+    {
+        return NumberStyles.AllowLeadingSign | NumberStyles.AllowDecimalPoint;
     }
 }
