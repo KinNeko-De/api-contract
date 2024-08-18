0fa37943d2b2493c7c0a67bdf8979712569154ef
Nils Diekmann <github@kinneko.de>
2024-08-18 19:41:44 +0200

    - add http annotatiions

---
+++ js/generate-js.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+protobase=proto
+protobasepackage=kinnekode
+protogoogle=proto/google
+protopath=${protobase}/${protobasepackage}
+
+# cd ${protobasepackage}
+# outdated=( $(\ls -d */) )
+# for o in "${outdated[@]}"
+# do
+#     find ${o} -name *.pb.go \
+#     -exec rm {} +
+#     echo "Removed generated js code for $o"
+# done
+# cd - > /dev/null
+
+cd ..
+
+cd ${protopath}
+projects=( $(\ls -d */ ) )
+cd - > /dev/null
+
+outputpath=js
+
+for i in "${projects[@]}"
+do
+    find ${protopath}/${i} -name *.proto \
+    -exec tools/protoc/win64/bin/protoc.exe --proto_path=${protobase} --js_out=import_style=commonjs:${outputpath} --grpc-web_out=import_style=typescript,mode=grpcwebtext:./generated {} +
+    echo "Generated js code for: $i"
+done
+
+read -p "Press [Enter] to exit."
